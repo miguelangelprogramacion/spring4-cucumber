bd2f064b54126103e0088ff8e2e3df665835cf15
Miguel Ángel Fernández Fernández <miguelangelprogramacion@gmail.com>
2015-09-26 11:37:21 +0200

    One step was added

--- src/test/java/world/we/deserve/ExistenceStepsdef.java
+++ src/test/java/world/we/deserve/ExistenceStepsdef.java
@@ -3,11 +3,19 @@
  */
 package world.we.deserve;
 
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+
 /**
  * @author Miguel Ángel Dev (miguelangelprogramacion@gmail.com)
  *
  */
 public class ExistenceStepsdef {
 
+	@Given("^I'm a human being in particular space-time$")
+	public void i_m_a_human_being_in_particular_space_time() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
 	
 }
