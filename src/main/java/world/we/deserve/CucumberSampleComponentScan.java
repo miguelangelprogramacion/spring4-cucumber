401fa6924a263bb2e4ae858e3cc7cdf80474f64a
Miguel Ángel Fernández Fernández <miguelangelprogramacion@gmail.com>
2015-09-26 10:46:00 +0200

    Create basic component scan. Remove default classes

---
+++ src/main/java/world/we/deserve/CucumberSampleComponentScan.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package world.we.deserve;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Miguel Ángel Dev (miguelangelprogramacion@gmail.com)
+ *
+ */
+@Configuration
+@ComponentScan
+public class CucumberSampleComponentScan {
+
+}
