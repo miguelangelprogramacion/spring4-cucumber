7d7872f4e282a0050f167a6f2b7ee9adc9343357
Miguel Ángel Fernández Fernández <miguelangelprogramacion@gmail.com>
2015-09-26 11:19:30 +0200

    Cucumber dependencies and basic RunCukes

---
+++ src/test/java/world/we/deserve/RunCukesTest.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package world.we.deserve;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+/**
+ * @author Miguel Ángel Dev (miguelangelprogramacion@gmail.com)
+ *
+ */
+@RunWith(Cucumber.class)
+@CucumberOptions(plugin = { "pretty" })
+public class RunCukesTest {
+
+}
